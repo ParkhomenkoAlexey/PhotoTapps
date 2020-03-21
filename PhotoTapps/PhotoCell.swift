21a18d476a3cc2f624d8d87c1c359af844240297
ParkhomenkoAlexey <Parkhomenko_AD@mail.ru>
2020-03-21 22:16:46 +0500

    14 урок. CollectionView Flow Layout Setup

---
+++ PhotoTapps/PhotoCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCell.swift
+//  PhotoTapps
+//
+//  Created by Алексей Пархоменко on 21.03.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    
+}
